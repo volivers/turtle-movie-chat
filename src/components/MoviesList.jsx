59e3afad22dca1973c239f9bba7dc10d8157d599
Vasco Oliveira <vascooliveira@outlook.com>
2021-01-26 10:47:26 +0000

    installed match sorter & finished filtering by title

--- src/components/MoviesList.jsx
+++ src/components/MoviesList.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import data from '../data/movies.json';
 import ReactTable from 'react-table-v6';
 import 'react-table-v6/react-table.css';
+import { matchSorter } from 'match-sorter';
 
 const MoviesList = () => {
 
@@ -9,19 +10,28 @@ const MoviesList = () => {
 
   useEffect(() => {
     setMovies(data);
-    // console.log(movies);
   },[])
 
   return (
     <div>
       <ReactTable
         data={movies}
+        defaultFilterMethod={(filter, row) =>
+          String(row[filter.id]) === filter.value}
         columns={[
           {
             Header: "Title",
             accessor: "title",
             width: 300,
-            filterable: true
+            filterable: true,
+            Filter: ({filter, onChange}) => (
+              <input type='text'
+                     placeholder="Filter by title"
+                     value={filter ? filter.value : ''}
+                     onChange={event => onChange(event.target.value)} />),
+            filterMethod: (filter, rows) =>
+              matchSorter(rows, filter.value, { keys: ["title"] }),
+            filterAll: true
           },
           {
             Header: "Year",
@@ -31,7 +41,10 @@ const MoviesList = () => {
           {
             Header: "Runtime",
             accessor: "runtime",
-            maxWidth: 150
+            maxWidth: 150,
+            Cell: row => (
+              row.value ? <span>{Math.floor(row.value / 60)}h {row.value % 60}min</span> : '-'
+            )
           },
           {
             Header: "Revenue",
